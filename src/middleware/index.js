35dc3e4a453b61346266dc25f08092477465fe2f
GeorgeMc1 <george.man1@hotmail.co.uk>
2023-01-10 16:18:47 +0000

    checkToken done

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -19,3 +19,24 @@ exports.hashPass = async (req, res, next) => {
         next();
     }
 }
+
+exports.checkToken = async (req, res, next) => {
+    try{
+        if(!req.header("Authorization")){
+            console.log("no token passed");
+            throw new Error("no token passed");
+        }
+        const token = req.header("Authorization").replace("Bearer ", "");
+        const decodedToken = await jwt.verify(token, procss.env.SECRET);
+        req.user = await User.findByIf(decodedToken._id);
+        if(req.user){
+            req.authUser = req.user;
+            next();
+        } else {
+            throw new Error("user is not authorised");
+        }
+    } catch(error){
+        console.log(error);
+        res.status(500).send({error: error.message});
+    }
+}
