5ad0c7d3b62d125eebc435330c6c23ae5f1c8532
Dave Hale <davejlhale@yahoo.co.uk>
2023-01-14 23:11:35 +0000

    limits

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit : 52428800}))
+app.use(express.urlencoded({limit:52428800, extended: true}));
 app.use(userRouter);
 
 app.get("/health", (req, res) => {
